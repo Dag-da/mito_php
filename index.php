11dd912fd0450fd634a2373cb8e4d4b6ed39a9c9
dag-da <sylvain.leflon@gmail.com>
2022-07-26 21:03:21 +0200

    index.html + hero

--- index.php
+++ index.php
@@ -3,6 +3,34 @@ $title = "Home";
 include("./partials/_header.php");
 ?>
 <main>
-allo
+    <section>
+        <h2>Qui sommes-nous ?</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptatem voluptas cum doloremque saepe perspiciatis voluptates cupiditate beatae, blanditiis tempora, eligendi iusto tenetur voluptate, animi hic quaerat? Repudiandae, amet sit, commodi ex minima consequuntur laudantium, qui quas et non quod.
+        </p>
+        <div class="">
+            <img src="assets/img/home-mito.jpeg" alt="Rayons fruits et légumes frais">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam minus veritatis nesciunt numquam! Laudantium ducimus nihil voluptates sed et, temporibus veniam odio nulla nisi numquam quibusdam dolor earum labore, praesentium vitae magnam perferendis nesciunt eius. Iste nam aliquid, nostrum excepturi soluta, porro commodi minima fugiat obcaecati earum quisquam quibusdam asperiores.
+            </p>
+        </div>
+    </section>
+    <section>
+        <h2>Nos services</h2>
+        <div class="flex">
+            <div class="">
+                <img src="assets/img/gift-box.png" alt="Emballage">
+                <p>Emballage</p>
+            </div>
+            <div class="">
+                <img src="assets/img/advice.png" alt="Livraison">
+                <p>Livraison</p>
+            </div>
+            <div class="">
+                <img src="assets/img/fast-delivery.png" alt="Conseils">
+                <p>Conseils</p>
+            </div>
+        </div>
+    </section>
 </main>
 <?php include("./partials/_footer.php") ?>
